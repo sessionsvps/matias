5e7c07033507a76ecc01a07d4e3a6b95182c12ba
sessionsvps <dejercito4545@gmail.com>
2025-04-22 23:30:44 -0500

    Add files via upload

--- matias.py
+++ matias.py
@@ -34,45 +34,56 @@ async def getMessagesFromGroup(client, group_id):
         return []
 async def logUserBot():
     load_dotenv()
-    api_id = int(20629704)
-    api_hash = "e82c77e92ae9c1d3ed99808f4022bbb6"
-    phone_number = "51975037427"
-    session_name = "matias_spammer"
+    api_id = int(29205034)
+    api_hash = "aedb2d993c160d60c1ac5d6d81003fa0"
+    phone_number = "51988041614"
+    session_name = "bot_spammer"
     client = TelegramClient(session_name, api_id, api_hash)
     await client.connect()
     if not await client.is_user_authorized():
         await client.send_code_request(phone_number)
         await client.sign_in(phone_number, input('Ingrese el código de verificación: '))
-    await client.send_message("@elspamxd", f'<b>Bot encendido</b>', parse_mode="HTML")
-    spammer_group = int("-4289413832")
+    await client.send_message("@spmmatias", f'<b>Bot encendido</b>', parse_mode="HTML")
+    spammer_group = int("-4687615477")
+
+    # @client.on(events.NewMessage)
+    # async def my_event_handler(event):
+    #     # Verificar si el mensaje proviene de un chat privado
+    #     if event.is_private:
+    #         sender = await event.get_sender()
+    #         sender_id = sender.id
+    #         message = event.message.message
+    #         # Responder solo en chats privados
+    #         await client.send_message(sender_id, "")
 
     while True:
         groups_info = await getListOfGroups(client)
         messages_list = await getMessagesFromGroup(client, spammer_group)
             
         try:
-            await client.send_message("@elspamxd", f"<b>CANTIDAD DE MENSAJES CONSEGUIDOS PARA PUBLICAR</b> <code>{len(messages_list)-1}</code>",parse_mode="HTML")
+            await client.send_message("@spmmatias", f"<b>CANTIDAD DE MENSAJES CONSEGUIDOS PARA PUBLICAR</b> <code>{len(messages_list)-1}</code>",parse_mode="HTML")
         except:
             pass
             
         try:
             for i in groups_info:
-                if i['group_name'] not in ["spam 2024","RESPALDO🇵🇪BINS PERU","QUEMANDO ESTAFADORES","Referencias Elmer 💸","🎭 CANAL MUNDO STREAMING PERÚ 🇵🇪","TU MARKETPLACE"]:
+                if i['group_name'] not in ["Spam"] and i['group_id'] not in[-1002351366917,-1002331326186,-1002425258747,-1002432211914,-1002216176545,-1001926519077,-1001580673964,-1001907073788,-1001737351681,-1001733869168,-1001829996546,-1002018025154,-1001859082953,-4553972318,-4553791708,-4559750226,-4586567990,-1001724620371,-1001760634472,-1002125807620,-1001617010310,-1001867739320,-1002074331354,-1001789640951,-1001899823705,-1001890531963,-1002296434091]:
                     j=0
                     for message_spam in messages_list:
                         j+=1
                         resultado = True
                         try:
-                            await client.send_message(i["group_id"], message_spam)
+                            await client.forward_messages(i["group_id"], message_spam)
                         except Exception as error:
-                            await client.send_message("@elspamxd", f'<b>Error enviando mensajes a  a {i["group_id"]}</b> - <code>{i["group_name"]}<code>\nCausa:{error}',parse_mode="HTML")
+                            await client.send_message("@spmmatias", f'<b>Error enviando mensajes a {i["group_id"]}</b> - <code>{i["group_name"]}<code>\nCausa:{error}',parse_mode="HTML")
                             resultado = False
                         if resultado:
-                            await client.send_message("@elspamxd", f'<b>Mensaje enviado a {i["group_id"]}</b> - <code>{i["group_name"]}</code>',parse_mode="HTML")  
-                        await asyncio.sleep(5)
+                            await client.send_message("@spmmatias", f'<b>Mensaje enviado a {i["group_id"]}</b> - <code>{i["group_name"]}</code>',parse_mode="HTML")  
+                        else: break
+                        await asyncio.sleep(90)
                         if j==1: break
-            await client.send_message("@elspamxd", f'<b>RONDA ACABADA</b>', parse_mode="HTML")
-            await asyncio.sleep(120) 
+            await client.send_message("@spmmatias", f'<b>RONDA ACABADA</b>', parse_mode="HTML")
+            await asyncio.sleep(300) 
         except:
             pass
     
